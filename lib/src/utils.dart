7f82b17dcc645c5cf215615d6a4b2654aad248f0
Win <synwe@yahoo.com>
2019-07-07 02:41:17 +0000

    Reorganise utils

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:path/path.dart' as p;
 
 Future<HttpResponse> jsonResponse(HttpRequest request, dynamic data) async {
   request.response.statusCode = HttpStatus.ok;
@@ -7,4 +8,26 @@ Future<HttpResponse> jsonResponse(HttpRequest request, dynamic data) async {
       ContentType("application", "json", charset: "utf-8");
   request.response.write(jsonEncode(data));
   return request.response;
+}
+
+Future<Map<String, List<Map<String, dynamic>>>> directoryListing(
+    Directory dir) async {
+  List contents = dir.listSync()..sort((a, b) => a.path.compareTo(b.path));
+  var dirs = <Map<String, String>>[];
+  var files = <Map<String, dynamic>>[];
+  for (var fileOrDir in contents) {
+    if (fileOrDir is Directory) {
+      var dir = Directory("${fileOrDir.path}");
+      dirs.add({
+        "name": p.basename(dir.path),
+      });
+    } else {
+      var file = File("${fileOrDir.path}");
+      files.add(<String, dynamic>{
+        "name": p.basename(file.path),
+        "size": file.lengthSync()
+      });
+    }
+  }
+  return {"files": files, "directories": dirs};
 }
