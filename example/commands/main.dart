d60b7d1463a931f3bc36b81bb5146b27e344ebf3
Win <synwe@yahoo.com>
2019-07-07 02:39:41 +0000

    Improve the examples

---
+++ example/commands/main.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:isohttpd/isohttpd.dart';
+
+Future<HttpResponse> handler(HttpRequest request, IsoLogger log) async {
+  log.debug("Hello from request handler");
+  return jsonResponse(request, {"response": "ok"});
+}
+
+Future<String> initHost() async {
+  final interfaces = await NetworkInterface.list(
+      includeLoopback: false, type: InternetAddressType.any);
+  return interfaces.first.addresses.first.address;
+}
+
+void main() async {
+  /// set routes
+  final defaultRoute = IsoRoute(path: "*", handler: handler);
+  final routes = <IsoRoute>[defaultRoute];
+  final router = IsoRouter(routes);
+
+  /// set host
+  String host = await initHost();
+
+  /// init runner
+  final iso = IsoHttpdRunner(host: host, router: router);
+
+  /// listen to logs
+  iso.logs.listen((dynamic data) => print("$data"));
+  iso.requestLogs.listen((dynamic data) => print("=> $data"));
+
+  /// run
+  print("Running the server in an isolate");
+  await iso.run(startServer: false);
+  iso.status();
+  iso.start();
+  await iso.onServerStarted;
+  iso.status();
+  await Future<Null>.delayed(Duration(seconds: 3));
+  iso.stop();
+  iso.status();
+  //iso.dispose();
+}
