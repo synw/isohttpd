37c6fd06a402f7d730c50c13afb3707eb976616b
synw <synw@yahoo.com>
2019-12-17 05:13:07 +0100

    Move types file to the top folder

---
+++ lib/src/types.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+import 'logger.dart';
+
+typedef IsoRequestHandler = Future<HttpResponse> Function(
+    HttpRequest request, IsoLogger log);
+
+/// The server commands
+enum HttpdCommand {
+  /// Start the server
+  start,
+
+  /// Stop the server
+  stop,
+
+  /// Check the server status
+  status
+}
+
+/// The error types
+enum ServerError {
+  /// The server is already started
+  alreadyStarted,
+
+  /// The server is not started
+  notRunning
+}
+
+/// The server status
+enum ServerStatus {
+  /// The server is ready
+  ready,
+
+  /// The server is started
+  started,
+
+  /// The server is stopped
+  stopped
+}
+
+/// The log level
+enum LogMessageClass {
+  /// Succes message
+  success,
+
+  /// Error message
+  error,
+
+  /// Warning message
+  warning
+}
+
+/// The request log type
+enum IsoLogType {
+  /// Debug level
+  debug,
+
+  /// Info level
+  info,
+
+  /// Warning level
+  warning,
+
+  /// Error level
+  error,
+}
+
+/// Server events
+enum IsoServerEventType {
+  /// Server initialization
+  initialization,
+
+  /// Server start
+  startServer,
+
+  /// Server stop
+  stopServer
+}
